7167c38828d70ddb30fd7053ebb4449283541fac
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-04-25 09:42:36 -0300

    refactor: modify image path

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const icon = document.getElementById('iconPwd');
 toggleBtn.addEventListener('click', () => {
     const isHidden = pwdInput.type === 'password';
     pwdInput.type = isHidden ? 'text' : 'password';
-    icon.src = isHidden ? '/assets/eye-off.svg' : '/assets/eye.svg';
+    icon.src = isHidden ? 'assets/eye-off.svg' : 'assets/eye.svg';
     toggleBtn.setAttribute('aria-label',
         isHidden ? "Ocultar senha" : "Mostrar senha"
     );
